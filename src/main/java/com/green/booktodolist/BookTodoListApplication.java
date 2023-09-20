44e71481ebc8c23fb843f85c2c942fa687bb61e1
returnys <130682309+returnys@users.noreply.github.com>
2023-09-20 09:45:08 +0900

    Update BookTodoListApplication.java

--- src/main/java/com/green/booktodolist/BookTodoListApplication.java
+++ src/main/java/com/green/booktodolist/BookTodoListApplication.java
@@ -2,11 +2,25 @@ package com.green.booktodolist;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class BookTodoListApplication {
     public static void main(String[] args) {
         SpringApplication.run(BookTodoListApplication.class, args);
     }
-
+    @Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**")
+						.allowedHeaders("*") // 어떤 헤더들을 허용할 것인지
+						.allowedMethods("*") // 어떤 메서드를 허용할 것인지 (GET, POST…)
+						.allowedOrigins("https://web-travel-todo-cu6q2blkafu2i7.sel4.cloudtype.app");
+			}
+		};
+	}
 }
